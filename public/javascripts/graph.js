f5b3111a449a1b3ce40b1694d1dd69fd7760da1d
Travis <mail@travisbloomfield.com>
2016-03-15 13:04:09 +1100

    Changed to area graph.

--- public/javascripts/graph.js
+++ public/javascripts/graph.js
@@ -1,32 +1,40 @@
+var height = 500;
+var width = 500;
 // Load data
 var data = [];
 d3.json("/api/lakes/levels/System", function(err, d) {
   if (err) console.log(err);
-  data = d.map(function(e) {
-    return e.level;
-  });
+  data = d;
 
-  var xScale = d3.scale.linear();
+  var xScale = d3.time.scale();
   var yScale = d3.scale.linear();
 
   xScale.range([0, 500]);
-  xScale.domain([0, data.length]);
-  yScale.range([500, 0]);
-  yScale.domain(d3.extent(data));
+  xScale.domain(d3.extent(data, function(d) {
+    return d.date;
+  }));
+  yScale.range([height, 0]);
+  yScale.domain([0, d3.max(data, function(d) {
+    return d.level;
+  })]);
 
-  var svg = d3.select("svg");
 
-  var circle = svg.selectAll("circle")
-    .data(data);
+  var svg = d3.select("svg")
+    .attr("height", height)
+    .attr("width", width);
 
-  var circleEnter = circle.enter().append("circle");
+  var area = d3.svg.area()
+    .x(function(d) {
+      return xScale(d.date);
+    })
+    .y0(height)
+    .y1(function(d) {
+      return yScale(d.level);
+    });
 
-  circleEnter.attr("r", 3);
-  circleEnter.attr("cx", function(d, i) {
-    return xScale(i);
-  });
-  circleEnter.attr("cy", function(d) {
-    return yScale(d);
-  });
+  svg.append("path")
+    .datum(data)
+    .attr("d", area)
+    .attr("fill", "#2A8BE8");
 
 });
