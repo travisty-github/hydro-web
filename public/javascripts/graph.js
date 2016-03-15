1ef2723741943e07e820d5dfab50e350744cfd55
Travis <mail@travisbloomfield.com>
2016-03-15 15:06:04 +1100

    Added axes.

--- public/javascripts/graph.js
+++ public/javascripts/graph.js
@@ -1,33 +1,46 @@
 var height = 500;
-var width = 500;
+var width = 800;
+var margin = {top: 20, right: 20, bottom: 30, left: 50};
+var plotArea = {};
+plotArea.xRange= [0, width - margin.left - margin.right];
+plotArea.yRange= [height - margin.top - margin.bottom, 0];
 // Load data
 var data = [];
-d3.json("/api/lakes/levels/System", function(err, d) {
+d3.json("/api/lakes/levels/Lake%20St.%20Clair%20%2F%20Lake%20King%20William", function(err, d) {
   if (err) console.log(err);
   data = d;
 
   var xScale = d3.time.scale();
   var yScale = d3.scale.linear();
 
-  xScale.range([0, 500]);
+  xScale.range(plotArea.xRange);
   xScale.domain(d3.extent(data, function(d) {
     return d.date;
   }));
-  yScale.range([height, 0]);
+  yScale.range(plotArea.yRange);
   yScale.domain([0, d3.max(data, function(d) {
     return d.level;
   })]);
 
+  var xAxis = d3.svg.axis()
+    .scale(xScale)
+    .orient("bottom");
+
+  var yAxis = d3.svg.axis()
+    .scale(yScale)
+    .orient("left");
 
   var svg = d3.select("svg")
-    .attr("height", height)
-    .attr("width", width);
+    .attr("height", height )
+    .attr("width", width )
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   var area = d3.svg.area()
     .x(function(d) {
       return xScale(d.date);
     })
-    .y0(height)
+    .y0(plotArea.yRange[0])
     .y1(function(d) {
       return yScale(d.level);
     });
@@ -35,6 +48,13 @@ d3.json("/api/lakes/levels/System", function(err, d) {
   svg.append("path")
     .datum(data)
     .attr("d", area)
-    .attr("fill", "#2A8BE8");
+    .attr("fill", "#2A93E8");
+
+  svg.append("g")
+    .attr("transform", "translate(0," + plotArea.yRange[0] + ")")
+    .call(xAxis);
+
+  svg.append("g")
+    .call(yAxis);
 
 });
