e4e961fcdbbaa61a868ad1ee9146ab097b50b6bc
Travis <bloomfield.travis@gmail.com>
2016-03-17 16:19:29 +1100

    Expanding parts of chart on hover.

--- public/javascripts/treemap.js
+++ public/javascripts/treemap.js
@@ -1,5 +1,6 @@
 var height = 800;
-var width = 800;
+// var width = 800;
+var width = parseInt(d3.select("#chart").attr);
 var margin = {
   top: 20,
   right: 20,
@@ -11,10 +12,30 @@ plotArea.xRange = [0, width - margin.left - margin.right];
 plotArea.yRange = [height - margin.top - margin.bottom, 0];
 // Load data
 var data = [];
-d3.json("/api/lakes/currentlevels", function(err, data) {
+
+// var color = d3.scale.category20c();
+var colour = d3.scale.linear().domain([0, 30, 70]).range(["#FF0000", "#FF7D00", "#00FF00"]);
+
+var treemap = d3.layout.treemap()
+  .size([width, height])
+  .sticky(true)
+  .value(function(d) {
+    return d.capacity;
+  });
+
+var div = d3.select("#chart")
+  .style("position", "relative")
+  .style("width", (width + margin.left + margin.right) + "px")
+  .style("height", (height + margin.top + margin.bottom) + "px")
+  .style("left", margin.left + "px")
+  .style("top", margin.top + "px");
+
+var node = null;
+
+d3.json("/api/lakes/currentlevels", function(err, d) {
   if (err) console.log(err);
 
-  data = data.filter(function(d) {
+  data = d.filter(function(d) {
     return d.name !== 'System';
   });
 
@@ -22,37 +43,69 @@ d3.json("/api/lakes/currentlevels", function(err, data) {
     children: data
   };
 
-  var treemap = d3.layout.treemap()
-    .size([width, height])
-    .sticky(true)
-    .value(function(d) {
-      return d.capacity;
-    });
+  redraw();
 
-  var div = d3.select("#chart")
-    .style("position", "relative")
-    .style("width", (width + margin.left + margin.right) + "px")
-    .style("height", (height + margin.top + margin.bottom) + "px")
-    .style("left", margin.left + "px")
-    .style("top", margin.top + "px");
+});
 
+var redraw = function() {
+  width = window.innerWidth * 0.8;
 
+  treemap.size([width, height]);
 
-  var node = div.datum(data).selectAll(".node")
+  node = div.datum(data).selectAll(".node")
     .data(treemap.nodes)
     .enter().append("div")
     .attr("class", "node")
-    .call(position);
+    .on("mouseover", function(d) {
+      var e = d3.select(this);
+      var width = e.attr("enlarged-width");
+      var height = e.attr("enlarged-height");
+      e.style("width", width);
+      e.style("height", height);
+      e.classed("node-shadow", true);
+      e.select(".lake-name").style("font-size", fontSize(width, height));
+    })
+    .on("mouseout", function(d) {
+      var e = d3.select(this);
+      var width = e.attr("original-width");
+      var height = e.attr("original-height");
+      e.style("width", width);
+      e.style("height", height);
+      e.classed("node-shadow", false);
+      e.select(".lake-name").style("font-size", fontSize(width, height));
+    })
+    .call(position)
+    .attr("original-width", function(d) {
+      return d3.select(this).style("width");
+    })
+    .attr("original-height", function(d) {
+      return d3.select(this).style("height");
+    })
+    .attr("enlarged-width", function(d) {
+      var e = d3.select(this);
+      var width = e.style("width").slice(0, -2);
+      var enlargedWidth = width > 200 ? width * 1.1 : 200;
+      return enlargedWidth + "px";
+    })
+    .attr("enlarged-height", function(d) {
+      var e = d3.select(this);
+      var height = e.style("height").slice(0, -2);
+      var enlargedHeight = (height > 200) ? height * 1.1 : 200;
+      return enlargedHeight + "px";
+    });
 
   node.append("div")
     .attr("class", "lake-name")
-    .text(function(d) {
-      return d.name;
+    .html(function(d) {
+      if (!d.hasOwnProperty("name")) return;
+      var percentage = Math.round(d.currentLevel / d.capacity * 1000) / 10;
+      return d.name + "<br/>" + percentage + "%<br/>" + Math.round(d.currentLevel) + "GWh";
+      // return d.name;
     })
-    .style("font-size", function(d) {
+    .style("font-size", function(d) {return fontSize(d.dx + "px", d. dy + "px");}); // function(d) {
       // compute font size based on sqrt(area)
-      return Math.max(10, 0.1 * Math.sqrt(d.area)) + 'px';
-    });
+      // return Math.max(10, 0.1 * Math.sqrt(d.area)) + 'px';
+    // });
 
   node.append("div")
     .attr("class", "internal-graph")
@@ -62,6 +115,7 @@ d3.json("/api/lakes/currentlevels", function(err, data) {
     .style("margin-top", function(d) {
       return d.dy * (1 - (d.currentLevel / d.capacity)) + "px";
     });
+
 
   function position() {
     this.style("left", function(d) {
@@ -77,5 +131,14 @@ d3.json("/api/lakes/currentlevels", function(err, data) {
         return Math.max(0, d.dy - 1) + "px";
       });
   }
+};
 
-});
+var fontSize = function(width, height) {
+      width = width.slice(0, -2);
+      height = height.slice(0, -2);
+      console.log("width: " + width + " height: " + height);
+
+      return Math.max(10, 0.1 * Math.sqrt(width * height)) + 'px';
+      // return Math.max(10, 0.1 * Math.sqrt(d.area)) + 'px';
+};
+// d3.select(window).on("resize", redraw());
