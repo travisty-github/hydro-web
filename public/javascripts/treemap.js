200805f9a04288b29c6c68e2c414733d2f3b968f
Travis <bloomfield.travis@gmail.com>
2016-03-17 17:43:06 +1100

    Completed implementation of node scaling.

--- public/javascripts/treemap.js
+++ public/javascripts/treemap.js
@@ -1,6 +1,6 @@
 var height = 800;
 // var width = 800;
-var width = parseInt(d3.select("#chart").attr);
+var width = parseInt(d3.select('#chart').attr);
 var margin = {
   top: 20,
   right: 20,
@@ -13,9 +13,6 @@ plotArea.yRange = [height - margin.top - margin.bottom, 0];
 // Load data
 var data = [];
 
-// var color = d3.scale.category20c();
-var colour = d3.scale.linear().domain([0, 30, 70]).range(["#FF0000", "#FF7D00", "#00FF00"]);
-
 var treemap = d3.layout.treemap()
   .size([width, height])
   .sticky(true)
@@ -23,16 +20,16 @@ var treemap = d3.layout.treemap()
     return d.capacity;
   });
 
-var div = d3.select("#chart")
-  .style("position", "relative")
-  .style("width", (width + margin.left + margin.right) + "px")
-  .style("height", (height + margin.top + margin.bottom) + "px")
-  .style("left", margin.left + "px")
-  .style("top", margin.top + "px");
+var div = d3.select('#chart')
+  .style('position', 'relative')
+  .style('width', (width + margin.left + margin.right) + 'px')
+  .style('height', (height + margin.top + margin.bottom) + 'px')
+  .style('left', margin.left + 'px')
+  .style('top', margin.top + 'px');
 
 var node = null;
 
-d3.json("/api/lakes/currentlevels", function(err, d) {
+d3.json('/api/lakes/currentlevels', function(err, d) {
   if (err) console.log(err);
 
   data = d.filter(function(d) {
@@ -52,93 +49,110 @@ var redraw = function() {
 
   treemap.size([width, height]);
 
-  node = div.datum(data).selectAll(".node")
+  node = div.datum(data).selectAll('.node')
     .data(treemap.nodes)
-    .enter().append("div")
-    .attr("class", "node")
-    .on("mouseover", function(d) {
+    .enter().append('div')
+    .attr('class', function(d) {
+      /*** Add in additional data properties here. ***/
+      d.percentFull = d.currentLevel / d.capacity;
+      d.dxLarge = d.dx * 1.1 > 200 ? d.dx * 1.1 : 200;
+      d.dyLarge = d.dy * 1.1 > 200 ? d.dy * 1.1 : 200;
+      /***/
+      return 'node';
+    })
+    .on('mouseover', function(d) {
       var e = d3.select(this);
-      var width = e.attr("enlarged-width");
-      var height = e.attr("enlarged-height");
-      e.style("width", width);
-      e.style("height", height);
-      e.classed("node-shadow", true);
-      e.select(".lake-name").style("font-size", fontSize(width, height));
+      e.style('width', function(d) {
+        return d.dxLarge + 'px';
+      });
+      e.style('left', function(d) {
+        return (d.x - (d.dxLarge - d.dx) / 2) + 'px';
+      });
+      e.style('height', function(d) {
+        return d.dyLarge + 'px';
+      });
+      e.style('top', function(d) {
+        return (d.y - (d.dyLarge - d.dy) / 2) + 'px';
+      });
+
+      e.classed('node-shadow', true);
+
+      e.select('.lake-name').style('font-size', function(d) {
+        return fontSize(d.dxLarge, d.dyLarge);
+      });
+
+      e.select('.internal-graph')
+        .style('height', function(d) {
+          return d.dyLarge * (d.percentFull) + 'px';
+        })
+        .style('margin-top', function(d) {
+          return d.dyLarge * (1 - (d.percentFull)) + 1 + 'px';
+        });
     })
-    .on("mouseout", function(d) {
+    .on('mouseout', function(d) {
       var e = d3.select(this);
-      var width = e.attr("original-width");
-      var height = e.attr("original-height");
-      e.style("width", width);
-      e.style("height", height);
-      e.classed("node-shadow", false);
-      e.select(".lake-name").style("font-size", fontSize(width, height));
+      e.style('width', function(d) {
+        return d.dx + 'px';
+      });
+      e.style('left', function(d) {
+        return d.x + 'px';
+      });
+      e.style('height', function(d) {
+        return d.dy + 'px';
+      });
+      e.style('top', function(d) {
+        return d.y + 'px';
+      });
+      e.classed('node-shadow', false);
+      e.select('.lake-name').style('font-size', function(d) {
+        return fontSize(d.dx, d.dy);
+      });
+      e.select('.internal-graph')
+        .style('height', function(d) {
+          return d.dy * (d.percentFull) + 'px';
+        })
+        .style('margin-top', function(d) {
+          return d.dy * (1 - (d.percentFull)) + 1 + 'px';
+        });
     })
-    .call(position)
-    .attr("original-width", function(d) {
-      return d3.select(this).style("width");
+    .call(position);
+
+  node.append('div')
+    .attr('class', 'lake-name')
+    .html(function(d) {
+      if (!d.hasOwnProperty('name')) return;
+      return d.name + '<br/>' + Math.round(d.percentFull * 1000) / 10 + '%<br/>' + Math.round(d.currentLevel) + 'GWh';
     })
-    .attr("original-height", function(d) {
-      return d3.select(this).style("height");
-    })
-    .attr("enlarged-width", function(d) {
-      var e = d3.select(this);
-      var width = e.style("width").slice(0, -2);
-      var enlargedWidth = width > 200 ? width * 1.1 : 200;
-      return enlargedWidth + "px";
-    })
-    .attr("enlarged-height", function(d) {
-      var e = d3.select(this);
-      var height = e.style("height").slice(0, -2);
-      var enlargedHeight = (height > 200) ? height * 1.1 : 200;
-      return enlargedHeight + "px";
+    .style('font-size', function(d) {
+      return fontSize(d.dx, d.dy);
     });
 
-  node.append("div")
-    .attr("class", "lake-name")
-    .html(function(d) {
-      if (!d.hasOwnProperty("name")) return;
-      var percentage = Math.round(d.currentLevel / d.capacity * 1000) / 10;
-      return d.name + "<br/>" + percentage + "%<br/>" + Math.round(d.currentLevel) + "GWh";
-      // return d.name;
+  node.append('div')
+    .attr('class', 'internal-graph')
+    .style('height', function(d) {
+      return d.dy * (d.percentFull) + 'px';
     })
-    .style("font-size", function(d) {return fontSize(d.dx + "px", d. dy + "px");}); // function(d) {
-      // compute font size based on sqrt(area)
-      // return Math.max(10, 0.1 * Math.sqrt(d.area)) + 'px';
-    // });
-
-  node.append("div")
-    .attr("class", "internal-graph")
-    .style("height", function(d) {
-      return d.dy * (d.currentLevel / d.capacity) + "px";
-    })
-    .style("margin-top", function(d) {
-      return d.dy * (1 - (d.currentLevel / d.capacity)) + "px";
+    .style('margin-top', function(d) {
+      return d.dy * (1 - (d.percentFull)) + 'px';
     });
 
 
   function position() {
-    this.style("left", function(d) {
-        return d.x + "px";
+    this.style('left', function(d) {
+        return d.x + 'px';
       })
-      .style("top", function(d) {
-        return d.y + "px";
+      .style('top', function(d) {
+        return d.y + 'px';
       })
-      .style("width", function(d) {
-        return Math.max(0, d.dx - 1) + "px";
+      .style('width', function(d) {
+        return Math.max(0, d.dx - 1) + 'px';
       })
-      .style("height", function(d) {
-        return Math.max(0, d.dy - 1) + "px";
+      .style('height', function(d) {
+        return Math.max(0, d.dy - 1) + 'px';
       });
   }
 };
 
 var fontSize = function(width, height) {
-      width = width.slice(0, -2);
-      height = height.slice(0, -2);
-      console.log("width: " + width + " height: " + height);
-
-      return Math.max(10, 0.1 * Math.sqrt(width * height)) + 'px';
-      // return Math.max(10, 0.1 * Math.sqrt(d.area)) + 'px';
+  return Math.max(10, 0.12 * Math.sqrt(width * height)) + 'px';
 };
-// d3.select(window).on("resize", redraw());
