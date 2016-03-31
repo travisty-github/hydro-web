54bd788a269e58d08dd447c976eb93701f25b838
Travis <bloomfield.travis@gmail.com>
2016-03-31 11:09:06 +1100

    Source data from mongodb.

--- controllers/lakedata.js
+++ controllers/lakedata.js
@@ -1,75 +1,116 @@
 var fs = require('fs');
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://localhost:27017/hydro-data', ['lakes']);
 
-function LakeData(fileName) {
-  try {
-    fs.accessSync(fileName, fs.R_OK, function(err) {
-      console.log('Cannot read file' + fileName);
-      throw new Error(err);
-    });
-  } catch (err) {
-    console.log('Failed to open ' + fileName);
-    console.log(err);
-    throw new Error(err);
-  }
-
-  this.jsonData = fs.readFileSync(fileName);
-  try {
-    this.data = JSON.parse(this.jsonData);
-  } catch (err) {
-    console.log('Failed to parse data file.');
-    console.log(err);
-    throw new Error(err);
-  }
-}
+function LakeData(url) {}
 // Gets the lake object corresponding to lakeName. If not found null is returned.
 // Lake to be specified in encodedURI format.
-LakeData.prototype.getLake = function(lakeName) {
+LakeData.prototype.getLake = function(lakeName, callback) {
   var decodedLakeName = decodeURIComponent(lakeName);
-  var lake = this.data.filter(function(d) {
-    return decodedLakeName === d.name;
-  });
-
-  // Each lake name should be unique. If there are duplicates this is a problem.
-  if (lake.length > 1)
-    throw new Error('Duplicate lakes found.');
-
-  // If no matches found return null.
-  if (lake.length === 0) {
-    return null;
-  }
-
-  return lake[0];
+  db.lakes.find({
+    name: decodedLakeName
+  }, callback);
 };
 
 // Returns an array of objects containing all lake names and their corresponding
 // URI.
-LakeData.prototype.lakes = function() {
-  return this.data.map(function(d) {
-    return {
-      name: d.name,
-      uri: encodeURIComponent(d.name),
-      capacity: d.capacity
-    };
+LakeData.prototype.lakes = function(callback) {
+  db.lakes.find({
+    name: {
+      $exists: true
+    }
+  }, {
+    name: 1,
+    _id: 0
+  }, function(err, names) {
+    if (err) {
+      throw new Error(err);
+    }
+    var data = names.map(function(e) {
+      return {
+        name: e.name,
+        uri: encodeURIComponent(e.name)
+      };
+    });
+    callback(data);
   });
 };
 
 // Returns an array of the historical lake levels from an encoded URI of the
-// lake name
-LakeData.prototype.lakeLevels = function(lakeName) {
-  var lake = this.getLake(lakeName);
-  return lake.historicalLevels;
+// lake name. If no matches found returns an empty array.
+LakeData.prototype.lakeLevels = function(lakeName, callback) {
+  var lake = this.getLake(lakeName, function(err, data) {
+    if (data.length === 0) {
+      callback(data);
+    } else {
+      db.lakes.find({
+          lakeId: data[0]._id
+        }, {
+          _id: 0,
+          level: 1,
+          date: 1
+        },
+        function(err, data) {
+          if (err) throw new Error(err);
+          callback(data);
+        });
+    }
+  });
 };
 
 // Returns an array of the current lake levels for all lakes. Assumes that the
 // historical levels are sorted in ascending order.
-LakeData.prototype.currentLevels = function() {
-  return this.data.map(function(d) {
-    return {
-      name: d.name,
-      capacity: d.capacity,
-      currentLevel: d.historicalLevels.slice(-1)[0].level
-    };
-  });
+LakeData.prototype.currentLevels = function(callback) {
+
+  // Get all lake names for use later
+  db.lakes.find({
+      name: {
+        $exists: true
+      }
+    },
+    function(err, data) {
+      // Get all the latest lake levels.
+      if (err) throw new Error(err);
+      // Store returned data in object to enable easy lookup
+      var lakes = {};
+      data.forEach(function(d) {
+        lakes[d._id] = {
+          name: d.name,
+          capacity: d.capacity
+        };
+      });
+
+      db.lakes.aggregate({
+          $match: {
+            lakeId: {
+              $exists: true
+            }
+          }
+        }, {
+          $sort: {
+            date: -1
+          }
+        }, {
+          $group: {
+            _id: '$lakeId',
+            level: {
+              $first: '$level'
+            }
+          }
+        },
+        function(err, data) {
+          // Match the lake levels to the lake names by the ObjectId
+          var currentLakeLevels = [];
+          data.forEach(function(d) {
+            currentLakeLevels.push({
+              name: lakes[d._id].name,
+              level: d.level,
+              capacity: lakes[d._id].capacity
+            });
+          });
+          callback(currentLakeLevels);
+        });
+    });
 };
 
 module.exports = LakeData;
--- routes/lakes.js
+++ routes/lakes.js
@@ -2,54 +2,45 @@ var express = require('express');
 var router = express.Router();
 var LakeLevels = require('../controllers/lakedata.js');
 
-// Load lake levels
-// TODO: Error handling.
-var lakeLevels = new LakeLevels('./lakedata.json');
+var lakeLevels = new LakeLevels('url to go here');
 
 /* GET lakes listing. */
 router.get('/', function(req, res) {
-  res.json(lakeLevels.lakes());
+  lakeLevels.lakes(function(d) {
+    res.json(d);
+  });
 });
 
 /* Get all current levels */
 router.get('/currentlevels', function(req, res) {
-  res.json(lakeLevels.currentLevels());
+  lakeLevels.currentLevels(function(data) {
+    res.json(data);
+  });
 });
 
 /* GET lake historical levels. */
 router.get('/levels/:name', function(req, res) {
   var levels = null;
-  try {
-    levels = lakeLevels.lakeLevels(req.params.name);
-  } catch (err) {
-    console.log('Error getting levels.');
-    console.log(err);
-    res.redirect(500, '/');
-    return;
-  }
-
-  // If null is returned no matches were found.
-  if (levels === null) res.redirect(404, '/');
-
-  res.json(levels);
+  levels = lakeLevels.lakeLevels(req.params.name, function(levels) {
+    res.json(levels);
+  });
 });
 
 /* GET all information about a lake */
+// TODO
 router.get('/:name', function(req, res) {
   var lake = null;
   try {
-    lake = lakeLevels.getLake(req.params.name);
+    lakeLevels.getLake(req.params.name, function(err, data) {
+      res.json(data[0]);
+    });
   } catch (err) {
     console.log('Error getting lake.');
     console.log(err);
-    res.redirect(500, '/');
+    res.status(500).send('Error getting lake.');
     return;
   }
 
-  // If null is returned no matches were found.
-  if (lake === null) res.redirect(404, '/');
-
-  res.json(lake);
 });
 
 
