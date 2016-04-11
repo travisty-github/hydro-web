58720eae9002e3da909efcc8c43d3346a10af54c
Travis <mail@travisbloomfield.com>
2016-04-11 13:19:46 +1000

    Refactor header into own file. Relocate chart to index.

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-/* GET graph page. */
-router.get('/graph', function(req, res) {
-  res.render('graph.ejs');
-});
-
-/* GET treemap page. */
-router.get('/treemap', function(req, res) {
-  res.render('treemap.ejs');
+  res.render('index');
 });
 
 module.exports = router;
