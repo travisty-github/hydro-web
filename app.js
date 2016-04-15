cde61ad1094f72a9e9c0c507608737c925e6c89e
Travis <mail@travisbloomfield.com>
2016-04-15 14:42:30 +1000

    Add contact form page and validation.

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.use('/assets', express.static(__dirname + '/public/dist/'));
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var validator = require('validator');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,6 +10,44 @@ router.get('/', function(req, res, next) {
 /* GET about page. */
 router.get('/about', function(req, res, next) {
   res.render('about');
+});
+
+/* GET contact form page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contactform');
+});
+
+/* POST contact form. */
+router.post('/contact', function(req, res, next) {
+
+  console.log(req.body.query, req.body.email);
+  // Check correct fields sent for request body
+  var requiredFields =  [
+    {
+      name: 'email',
+      errorMessage: 'Missing email address in request.'
+    },
+    {
+      name: 'query',
+      errorMessage: 'Missing query message in request.'
+    }
+  ];
+
+  requiredFields.forEach(function(field) {
+    if (! req.body[field.name]) {
+      res.status(400)
+        .send(field.errorMessage);
+    }
+  });
+
+  // Validate email address
+  if (! validator.isEmail(req.body.email))
+  {
+    res.status(400)
+      .send('Invalid email address.');
+  }
+  var query = validator.escape(req.body.query);
+  res.send('ok');
 });
 
 module.exports = router;
