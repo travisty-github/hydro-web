2398d590bbc73ddb990bca4adb853468bdf50d01
Travis <mail@travisbloomfield.com>
2016-04-15 15:53:49 +1000

    Send contact email.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var validator = require('validator');
+var nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -46,9 +47,41 @@ router.post('/contact', function(req, res, next) {
     res.status(400)
       .send('Invalid email address.');
   }
+
+  var fromAddress = req.body.email;
   var query = validator.escape(req.body.query);
 
   // Send email
+  var emailConfig = {
+    host: config.app.email.host,
+    port: config.app.email.port,
+    secure: config.app.email.secure,
+    auth: {
+      user: config.app.email.user,
+      pass: config.app.email.password
+    }
+  };
+
+  var email = {
+    from: config.app.email.user,
+    to: config.app.email.user,
+    subject: 'Tasmania Hydro Dam Levels Enquiry',
+    text: 'Email from: ' + fromAddress + '\n\n' + query
+  };
+
+  var transporter = nodemailer.createTransport(emailConfig);
+  transporter.sendMail(email, function(err, info) {
+    if(err) {
+      console.log(err);
+      res.status(500)
+        .send('Error sending email.');
+        return;
+    }
+
+    console.log('Message sent: ' + info.response);
+
+  });
+
 
   res.send('ok');
 });
